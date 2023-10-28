cdf80683ac6ace9f0f46444b40beda3ffcf6fa58
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-10-28 13:26:56 +0200

    end of the training

--- js/utils/findComponentsByPath/index.js
+++ js/utils/findComponentsByPath/index.js
@@ -1,1 +1,2 @@
-export const findComponentByPath = (path, routes) => routes.find(r => r.path.match(new RegExp(`^\\${path}$`, 'gm'))) || undefined;
+export const findComponentByPath = (path, routes) =>
+  routes.find((r) => r.path.match(new RegExp(`^\\${path}$`, "gm"))) || undefined;
